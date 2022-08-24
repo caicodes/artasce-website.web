d9799645cc465f98753302d56d78330a2c967632
caicodes <caicodes1@gmail.com>
2022-08-24 14:03:55 -0600

    vuetified complete

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    vuetify
-  </div>
+  <v-layout style="z-index: 0">
+    <v-app-bar class="bg-purple-darken-2">
+      <nav class="mx-auto">
+        <NuxtLink class="v-btn mx-8 text-white" to="/">home</NuxtLink>
+        <NuxtLink class="v-btn mx-8 text-white" to="/about">about</NuxtLink>
+        <NuxtLink class="v-btn mx-8 text-white" to="/contact">contact</NuxtLink>
+        <NuxtLink class="v-btn mx-8 text-white" to="/styleguide"
+          >styleguide</NuxtLink
+        >
+      </nav>
+    </v-app-bar>
+    <v-navigation-drawer color="grey-darken-2"></v-navigation-drawer>
+    <v-navigation-drawer
+      color="grey-darken-2"
+      location="right"
+    ></v-navigation-drawer>
+    <v-main><NuxtPage /></v-main>
+  </v-layout>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
-import { defineNuxtConfig } from 'nuxt'
+// nuxt.config.ts
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  css: ["vuetify/lib/styles/main.sass"],
+  vuetify: {
+    customVariables: ["~/assets/variables.scss"],
+    treeShake: true,
+  },
+  build: {
+    transpile: ["vuetify"],
+  },
+  vite: {
+    define: {
+      "process.env.DEBUG": false,
+    },
+  },
+});
